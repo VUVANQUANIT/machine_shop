4284f4e1e386114031ceaa4c20e5cf4bb2a9875e
Vũ Văn Quân <quancn27@gmail.com>
2026-01-26 22:26:27 +0700

    Initial commit

---
+++ src/main/java/com/example/machinesshop/MachinesshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.machinesshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachinesshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MachinesshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/machinesshop/MachinesshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.machinesshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachinesshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
