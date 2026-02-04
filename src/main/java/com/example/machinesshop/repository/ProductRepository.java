f32fc9bea6affd4ebb368c6054f78dff608a203a
Vũ Văn Quân <quancn27@gmail.com>
2026-02-04 23:41:11 +0700

    Chỉnh sửa phần tìm kiếm theo filter

--- src/main/java/com/example/machinesshop/repository/ProductRepository.java
+++ src/main/java/com/example/machinesshop/repository/ProductRepository.java
@@ -5,6 +5,7 @@ import com.example.machinesshop.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,7 +17,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
 
         Page<Product> findByNameContainingIgnoreCase(String name, Pageable pageable);
         /**
